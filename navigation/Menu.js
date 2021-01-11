60a7386ac0e6af2b3cc0fe10bdb9046fd3b620b0
sadamdamz <sadamdamzzz@gmail.com>
2021-01-11 18:44:47 +0530

    update

---
+++ navigation/Menu.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Dimensions,
+  Image,
+  SafeAreaView
+} from "react-native";
+import { Block, Text, theme } from "galio-framework";
+import { useSafeArea } from "react-native-safe-area-context";
+import Images from "../constants/Images";
+import { DrawerItem as DrawerCustomItem, Icon } from "../components";
+
+import nowTheme from "../constants/Theme";
+
+const { width } = Dimensions.get("screen");
+
+function CustomDrawerContent({
+  drawerPosition,
+  navigation,
+  profile,
+  focused,
+  state,
+  ...rest
+}) {
+  const insets = useSafeArea();
+  const screens = [
+    "Home",
+    // "Components",
+    "Articles",
+    "Profile",
+    "Account",
+  ];
+  return (
+    <Block
+      style={styles.container}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <SafeAreaView/>
+      <Block style={styles.header}>
+        {/* <Image style={styles.logo} source={Images.Logo} /> */}
+        <Block right style={styles.headerIcon}>
+          <Icon
+            name="align-left-22x"
+            family="NowExtra"
+            size={15}
+            color={"white"}
+          />
+        </Block>
+      </Block>
+      <Block flex style={{ paddingLeft: 8, paddingRight: 14 }}>
+        <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+          {screens.map((item, index) => {
+            return (
+              <DrawerCustomItem
+                title={item}
+                key={index}
+                navigation={navigation}
+                focused={state.index === index ? true : false}
+              />
+            );
+          })}
+        <DrawerCustomItem title="LOGOUT" navigation={navigation}/>
+        </ScrollView>
+      </Block>
+    </Block>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    paddingHorizontal: 28,
+    paddingBottom: theme.SIZES.BASE,
+    paddingTop: theme.SIZES.BASE * 3,
+    justifyContent: "center"
+  },
+  headerIcon: {
+    marginTop: -20
+  },
+  logo: {
+    height: 40,
+    width: 37
+  }
+});
+
+export default CustomDrawerContent;
---
+++ screens/Construction.js
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  StatusBar,
+  Dimensions
+} from "react-native";
+import { Block, Button, Text, theme } from "galio-framework";
+
+const { height, width } = Dimensions.get("screen");
+
+import nowTheme from "../constants/Theme";
+import Images from "../constants/Images";
+
+class Pro extends React.Component {
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <Block flex style={styles.container}>
+        <StatusBar hidden />
+        <Block flex center>
+          <ImageBackground
+            source={Images.Construction}
+            style={{ height, width, zIndex: 1 }}
+          />
+        </Block>
+
+        <Block center flex space="between" style={styles.padded}>
+
+          <Block flex row>
+            <Text
+              color="white"
+              middle
+              size={26}
+              style={{ fontFamily: 'montserrat-regular' }}
+            >
+              Under Construction
+            </Text>
+          </Block>
+        </Block>
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.COLORS.BLACK
+  },
+  padded: {
+    top: 270,
+    paddingHorizontal: theme.SIZES.BASE * 2,
+    position: 'absolute',
+    bottom: theme.SIZES.BASE,
+    zIndex: 2
+  },
+  button: {
+    width: width - theme.SIZES.BASE * 4,
+    height: theme.SIZES.BASE * 3,
+    shadowRadius: 0,
+    shadowOpacity: 0
+  },
+  title: {
+    marginTop: "-5%"
+  },
+  subTitle: {
+    marginTop: 20
+  },
+  pro: {
+    backgroundColor: nowTheme.COLORS.BLACK,
+    paddingHorizontal: 8,
+    marginLeft: 3,
+    borderRadius: 4,
+    height: 22,
+    marginTop: 0
+  },
+  font: {
+    fontFamily: 'montserrat-bold'
+  }
+});
+
+export default Pro;
